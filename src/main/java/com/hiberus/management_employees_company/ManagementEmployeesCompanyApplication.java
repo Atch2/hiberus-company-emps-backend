726c5709c7bb0bb05b4d704e7f34d8b08dc9f8a3
ATCH <anthonyfabian212@gmail.com>
2025-05-15 23:17:37 -0500

    first commit

---
+++ src/main/java/com/hiberus/management_employees_company/ManagementEmployeesCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.hiberus.management_employees_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementEmployeesCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementEmployeesCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hiberus/management_employees_company/ManagementEmployeesCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiberus.management_employees_company;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementEmployeesCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
